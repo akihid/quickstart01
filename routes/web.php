3657233254ac7aaf7d5f67e9fd932db7875f2eed
sato akihide <akihide315@gmail.com>
2019-10-26 09:50:36 +0900

    Add: タスク作成処理まで

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,35 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Task;
+use Illuminate\Http\Request;
+
+// タスク一覧表示
+Route::get('/', function(){
+    return view('tasks');
+});
+
+// タスク追加
+Route::post('/task', function(Request $request){
+// Request $requestという記述でPOSTデータを取得する
+	$validator = Validator::make($request->all(),[
+		'name' => 'required|max:255',
+	]);
+	if ($validator->fails()){
+		return redirect('/')
+			->withInput()
+			->withErrors($validator);
+	}
+
+	//　タスク作成処理
+	$task = new Task;
+	$task->name = $request->name;
+	$task->save();
+
+	return redirect('/');
+});
+
+// タスク削除
+Route::delete('/task/{task}', function(Task $task){
+    //{task}という処理で消すタスクを特定しfunction(Task $task)に送る
 });
